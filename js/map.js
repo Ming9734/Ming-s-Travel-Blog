595de87a6164344d567ff8152b3e3dd939fc2679
Ming9734 <164885522+Ming9734@users.noreply.github.com>
2025-12-11 01:07:57 +0000

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,20 +1,44 @@
-const map = L.map('map').setView([50, 10], 4);
+// 初始化 Leaflet 地圖
+const map = L.map('map').setView([51.5, -0.12], 5);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 18
+  maxZoom: 19,
+  attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-fetch('data/posts.json')
-  .then(r => r.json())
-  .then(posts => {
-    posts.forEach(p => {
-      const marker = L.marker([p.lat, p.lng]).addTo(map);
+// 讀取地點資料
+fetch('data/places.geojson')
+  .then(res => res.json())
+  .then(data => {
+
+    data.features.forEach(f => {
+      const coords = f.geometry.coordinates;
+      const props = f.properties;
+
+      const marker = L.marker([coords[1], coords[0]]).addTo(map);
+
+      // hover 顯示 info-box
       marker.on('mouseover', () => {
-        document.getElementById('info-box').innerHTML = `
-          <h3>${p.title}</h3>
-          <p>${p.summary}</p>
-          <a href="post.html?id=${p.id}">Read more</a>
+        const box = document.getElementById('info-box');
+        box.innerHTML = `
+          <h3>${props.title}</h3>
+          <p>${props.summary}</p>
+          <img src="${props.img}" style="width:100%; border-radius:8px; margin:10px 0;">
+          <p><a href="${props.url}">Read More</a></p>
         `;
       });
+
+      // 滑鼠移開恢復預設
+      marker.on('mouseout', () => {
+        const box = document.getElementById('info-box');
+        box.innerHTML = `<p>Hover a marker to see details.</p>`;
+      });
+
+      // 點擊前往 Post
+      marker.on('click', () => {
+        window.location.href = props.url;
+      });
+
     });
+
   });
