e13e4d2fc0d3bf69f04fc3004a8e43ddf11cde63
Ming9734 <164885522+Ming9734@users.noreply.github.com>
2025-12-13 12:25:22 +0000

    Update post.js

--- js/post.js
+++ js/post.js
@@ -19,3 +19,29 @@ document.getElementById('enter-post')?.addEventListener('click', () => {
     if (navbar) navbar.classList.add('active');
   }, 600);
 });
+function initGallery(images) {
+  const gallery = document.querySelector('.post-gallery');
+  if (!gallery || !images?.length) return;
+
+  let index = 0;
+
+  gallery.innerHTML = `
+    <div class="gallery-frame">
+      <img src="${images[0]}" id="gallery-img">
+      <button class="gallery-btn prev">‹</button>
+      <button class="gallery-btn next">›</button>
+    </div>
+  `;
+
+  const img = gallery.querySelector('#gallery-img');
+
+  gallery.querySelector('.prev').onclick = () => {
+    index = (index - 1 + images.length) % images.length;
+    img.src = images[index];
+  };
+
+  gallery.querySelector('.next').onclick = () => {
+    index = (index + 1) % images.length;
+    img.src = images[index];
+  };
+}
