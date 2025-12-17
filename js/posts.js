77e111b3cbc9d0a0a99e7e4d42a3fd914f8fad01
Ming9734 <164885522+Ming9734@users.noreply.github.com>
2025-12-17 14:05:46 +0000

    Update posts.js

--- js/posts.js
+++ js/posts.js
@@ -3,6 +3,9 @@ fetch('data/posts.json')
   .then(posts => {
     const grid = document.getElementById('postsGrid');
 
+    posts.sort((a, b) => {
+  return a.country.localeCompare(b.country) || a.city.localeCompare(b.city);
+});
     posts.forEach(p => {
       const card = document.createElement('a');
       card.href = `post.html?id=${p.id}`;
@@ -12,6 +15,7 @@ fetch('data/posts.json')
         <img src="${p.image}">
         <div class="overlay">
           <h3>${p.title}</h3>
+          <p><strong>${p.city},${p.country}</strong></p>
           <p>${p.summary}</p>
           <span>Read more →</span>
         </div>
